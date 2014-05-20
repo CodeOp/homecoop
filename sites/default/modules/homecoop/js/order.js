01726151c45eaf6d5efb618c4de9c7ac48fda9fd
ayalas <ayala@code-op.org>
2014-05-20 20:01:28 +0300

    order page load

--- sites/default/modules/homecoop/js/order.js
+++ sites/default/modules/homecoop/js/order.js
@@ -4,15 +4,13 @@ function ConfirmDelete()
 }
 function MemberChange()
 {
-  <?php
-  if ($oRecord->ID > 0) { ?>
-    if (!confirm(decodeXml('Attention: you have chosen to move this order from one member to another. To complete the operation confirm this message box and save the order')))
-    {
-      document.getElementById("selMemberID").value = <?php echo $oRecord->MemberID; ?>;
-      return;
+  if (Drupal.settings.homecoop.MemberID > 0) {
+    if (!confirm(Drupal.settings.homecoop.ConfirmMemberChange)) {
+      document.getElementById("selMemberID").value = Drupal.settings.homecoop.MemberID;
+      return false;
     }
-  <?php } ?>
-  
-  document.getElementById("hidPostAction").value = <?php echo Order::POST_ACTION_MEMBER_CHANGE; ?>;
-  document.frmMain.submit();
+  }
+  document.getElementById("hidPostAction").value = Drupal.settings.homecoop.PostActionMemberChange;
+  document.homecoop_page_order_form.submit();
+  return true;
 }
